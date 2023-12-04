52d69b3570775837bab3f44df1f31b83d226b6de
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-04 12:46:17 +0530

    initial commit

---
+++ hike-swiftui/hike_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hike_swiftuiApp.swift
+//  hike-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct hike_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
