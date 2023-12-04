b1cbcef9c5825455d7cb74123fbaf65a4dc14233
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-04 14:08:21 +0530

    feat: added CardView and CustomBackgroundView

---
+++ hike-swiftui/Component/CardView.swift
@@ -0,0 +1,37 @@
+//
+//  CardView.swift
+//  hike-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 04/12/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            Color("ColorIndigoMedium"),
+                            Color("ColorSalmonLight")
+                        ],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                )
+                .frame(width: 256, height: 256)
+
+            Image("image-1")
+                .resizable()
+                .scaledToFit()
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
