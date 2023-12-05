79f66e141e110732f050565511722b03ffc0e96d
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-05 13:50:09 +0530

    feat: added Presentation Detents in bottom sheet

--- hike-swiftui/Component/CardView.swift
+++ hike-swiftui/Component/CardView.swift
@@ -53,6 +53,7 @@ struct CardView: View {
                         .sheet(isPresented: $isShowingSheet) {
                             SettingsView()
                                 .presentationDragIndicator(.visible)
+                                .presentationDetents([.medium, .large])
                         }
                     }
                     Text("Fun and enjoyable activities for friends and families.")
