1d61a5a55a14dbaca92a88c57fca89b6b2dc2301
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-05 11:30:02 +0530

    feat: added motion animation with random coordinates, size, scale, speed and delay

--- hike-swiftui/Component/CustomCircleView.swift
+++ hike-swiftui/Component/CustomCircleView.swift
@@ -10,25 +10,28 @@ import SwiftUI
 struct CustomCircleView: View {
     @State private var isAnimateGradient: Bool = false
     var body: some View {
-        Circle()
-            .fill(
-                LinearGradient(
-                    colors: [
-                        .customIndigoMedium,
-                        .customSalmomLight
-                    ],
-                    startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
-                    endPoint: isAnimateGradient ? .bottomTrailing : . topTrailing
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            .customIndigoMedium,
+                            .customSalmomLight
+                        ],
+                        startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
+                        endPoint: isAnimateGradient ? .bottomTrailing : . topTrailing
+                    )
                 )
-            )
-            .onAppear {
-                withAnimation(
-                    .linear(duration: 3.0)
-                    .repeatForever(autoreverses: true)) {
-                        isAnimateGradient.toggle()
+                .onAppear {
+                    withAnimation(
+                        .linear(duration: 3.0)
+                        .repeatForever(autoreverses: true)) {
+                            isAnimateGradient.toggle()
+                    }
                 }
-            }
-            .frame(width: 256, height: 256)
+            MotionAnimationView()
+        } //: ZStack
+        .frame(width: 256, height: 256)
     }
 }
 
